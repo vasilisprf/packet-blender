68981209c99e6b4197dee8d4a582e371856a1a6b
Viktor Stanchev <vikstrous@users.noreply.github.com>
2014-04-21 11:54:58 -0400

    No need for if'

--- PacketBlender/Doodad.hs
+++ PacketBlender/Doodad.hs
@@ -4,10 +4,6 @@ import Network.Pcap
 import System.IO
 import Control.Monad
 
-if' :: Bool -> a -> a -> a
-if' True  x _ = x
-if' False _ y = y
-
 callfun f = do
     ( p , q ) <- next f
     callfun2 f p q
